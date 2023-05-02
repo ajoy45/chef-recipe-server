37a19249ff5614382b7dce287550fc73e27afb39
ajoy45 <ajoypust@gmail.com>
2023-05-02 17:51:33 +0600

    data load

--- index.js
+++ index.js
@@ -1,10 +1,20 @@
 
 const express = require('express')
 const app = express()
+const cors = require('cors')
 const port = 3000
-
+app.use(cors())
+ const data=require('./data/data.json')
 app.get('/', (req, res) => {
   res.send('Hello World!')
+})
+app.get('/data',(req,res)=>{
+    res.send(data)
+})
+app.get('/data/:id', (req, res) => {
+    const id =parseInt( req.params.id);
+    const selectedNews = data.find(n => n.id === id);
+    res.send(selectedNews)
 })
 
 app.listen(port, () => {
